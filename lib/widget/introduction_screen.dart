1b830661a5e3218fd3bf7b72e77f7f4ca017d22c
dew1213 <103346967+dew1213@users.noreply.github.com>
2024-02-08 17:24:23 +0700

    Add files via upload

--- lib/widget/introduction_screen.dart
+++ lib/widget/introduction_screen.dart
@@ -7,60 +7,67 @@ class IntroScreen extends StatelessWidget {
   IntroScreen({super.key});
   final List<PageViewModel> pages = [
     PageViewModel(
-        title: 'ORDER',
-        body: 'Begin your shopping journey with us!  ',
-        footer: SizedBox(
-          height: 45,
-          width: 150,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  elevation: 8,
-                ),
-                onPressed: () {},
-                child: const Text("Lets meow"),
+      title: 'ORDER',
+      body: 'Begin your shopping journey with us!  ',
+      footer: SizedBox(
+        height: 45,
+        width: 150,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                elevation: 8,
               ),
-            ],
-          ),
+              onPressed: () {},
+              child: const Text("Let's get started"),
+            ),
+          ],
         ),
-        image: Image.asset("assets/images/image12123.png"),
-        decoration: const PageDecoration(
-            titleTextStyle: TextStyle(
+      ),
+      image: Image.asset("assets/images/2.png"),
+      decoration: const PageDecoration(
+        imagePadding: EdgeInsets.all(16.0),
+        titleTextStyle: TextStyle(
           fontSize: 25,
           fontWeight: FontWeight.bold,
-        ))),
+        ),
+        pageColor: Color.fromARGB(255, 183, 181, 106),
+      ),
+    ),
     PageViewModel(
-        title: 'PAYMENT',
-        body:
-            'Secure your purchases! Add your payment details and enjoy worry-free transactions.',
-        footer: SizedBox(
-          height: 45,
-          width: 150,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  elevation: 8,
-                ),
-                onPressed: () {},
-                child: const Text("Lets meow"),
+      title: 'PAYMENT',
+      body:
+          'Secure your purchases! Add your payment details and enjoy worry-free transactions.',
+      footer: SizedBox(
+        height: 45,
+        width: 150,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                elevation: 8,
               ),
-            ],
-          ),
+              onPressed: () {},
+              child: const Text("Let's get started"),
+            ),
+          ],
         ),
-        image: Image.asset("assets/images/imag23e.png"),
-        decoration: const PageDecoration(
-            titleTextStyle: TextStyle(
-          fontSize: 25,
-          fontWeight: FontWeight.bold,
-        ))),
+      ),
+      image: Image.asset("assets/images/1.png"),
+      decoration: const PageDecoration(
+          imagePadding: EdgeInsets.all(16.0),
+          pageColor: Color.fromARGB(255, 88, 169, 185),
+          titleTextStyle: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          )),
+    ),
     PageViewModel(
         title: 'DELIVERY',
         body: 'Choose your delivery options to get started.',
@@ -77,44 +84,62 @@ class IntroScreen extends StatelessWidget {
                   elevation: 8,
                 ),
                 onPressed: () {},
-                child: const Text("Lets meow"),
+                child: const Text("Let's get started"),
+                
               ),
             ],
           ),
         ),
-        image: Image.asset("assets/images/imag1231223e.png"),
+        image: Image.asset("assets/images/3.png"),
         decoration: const PageDecoration(
+            imagePadding: EdgeInsets.all(16.0),
+            pageColor: Color.fromARGB(255, 118, 185, 121),
             titleTextStyle: TextStyle(
-          fontSize: 25,
-          fontWeight: FontWeight.bold,
-        ))),
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ))),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 109, 108, 80),
         title: const Text("Welcome to wet tamarind company"),
         centerTitle: true,
       ),
       body: IntroductionScreen(
         pages: pages,
         showSkipButton: true,
-        skip: const Text("skip"),
+        skip: const Text(
+          "skip",
+          style: TextStyle(
+            color: Color.fromARGB(255, 0, 0, 0), // กำหนดสีของข้อความเป็นสีแดง
+          ),
+        ),
         showDoneButton: true,
-        done: const Text("done"),
+        done: const Text(
+          "done",
+          style: TextStyle(
+            color: Color.fromARGB(255, 0, 0, 0), // กำหนดสีของข้อความเป็นสีแดง
+          ),
+        ),
         showNextButton: true,
         next: const Icon(
           Icons.arrow_forward,
           size: 25,
+          color: Colors.black,
         ),
         onDone: () => onDone(context), // ต้องการ function
         curve: Curves.bounceOut,
-        dotsDecorator: const DotsDecorator(
-          size: Size(15, 15),
-          color: Colors.blue,
-          activeColor: Colors.orange,
-          activeSize: Size.square(20),
+        dotsDecorator: DotsDecorator(
+          size: const Size.square(9),
+          color: const Color.fromARGB(255, 0, 0, 0),
+          activeColor: const Color.fromARGB(255, 134, 107, 68),
+          activeSize: const Size(18.0, 9.0),
+          activeShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          ),
         ),
       ),
     );
